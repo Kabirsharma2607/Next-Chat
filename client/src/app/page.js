90eaa7d2fb6b36b24fa81531e2f86b2331edddf0
Kabirsharma2607 <kabirsharma2607@gmail.com>
2023-10-12 00:53:47 +0530

    Just setting up the things

---
+++ client/src/app/page.js
@@ -0,0 +1,11 @@
+import Image from "next/image";
+import Home from "@/pages/Home";
+
+export default function page() {
+  return (
+    <div className="bg-red-800">
+      Home
+      <Home />
+    </div>
+  );
+}
